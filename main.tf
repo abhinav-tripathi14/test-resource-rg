64e64bc1cd9d3ecbeb8f3fe157cfd43abbc41517
Abhinav Tripathi <abhi.tripathi14@gmail.com>
2024-06-07 09:09:00 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,12 +1,12 @@
 provider "azurerm" {
   features {}
 }
-/*
+
 resource "azurerm_resource_group" "rg" {
   name     = "example-resources"
 location = "East US"
 }
-*/
+
 
 resource "null_resource" "test" {
 count=2
